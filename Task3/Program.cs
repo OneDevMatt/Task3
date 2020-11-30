1ba15192cf9aae7c1f9eac591dbe047f9d83c6c0
Matthew Igbo <mattew.igbo@thebulb.africa>
2020-11-30 16:56:27 +0100

    Add project files.

---
+++ Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WordChecker();
+        }
+
+
+
+        static void WordChecker()
+        {
+            Console.WriteLine("Enter a word");
+            string word = Console.ReadLine();
+            if (word.Contains("s" + "a"))
+            {
+                word = word.Replace("e", "");
+                word = word.Replace("m", "");
+                for (int i = 0; i < word.Length; i++)
+                {
+                    Console.WriteLine(word[i]); 
+                }
+             Console.WriteLine(word.Length);
+            }
+            else
+            {
+                Console.WriteLine(word);
+            }
+            
+        }
+    }
+}
